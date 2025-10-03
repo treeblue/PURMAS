02c76fef2b6a1e332f8b1c976f422cacbbb68b79
HFullwood <treeblue3@gmail.com>
2025-10-03 18:36:04 +0100

    fleshed out comms.internode

--- src/comms.py
+++ src/comms.py
@@ -17,7 +17,7 @@ class intranode:
     def listen(self, n:int=1):
         self.sock.listen(n)
 
-    def accept(self)->str:
+    def accept(self):
         self.conn, _ = self.sock.accept()
 
     #client
@@ -45,8 +45,47 @@ class intranode:
 
 
 class internode:
-    def __init__(self):
-        self.socket = ""
+    def __init__(self, host:str = '', server:bool = False):
+        self.sock_loc = host
+        self.server = server
+        self.port = 25732
+
+    #general
+    def start(self):
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    #server
+    def bind(self):
+        self.sock.bind((self.sock_loc,self.port))
+
+    def listen(self, n:int=1):
+        self.sock.listen(n)
+
+    def accept(self)->str:
+        self.conn, _ = self.sock.accept()
+
+    #client
+    def connect(self):
+        self.sock.connect((self.sock_loc,self.port))
+
+    #communication
+    def write(self,message:str):
+        if self.server:
+            self.conn.sendall(message.encode())
+        else:
+            self.sock.sendall(message.encode())
+
+    def read(self)->str:
+        if self.server:
+            data = self.conn.recv(1024).decode()
+            return data
+        else:
+            data = self.sock.recv(1024).decode()
+            return data
+
+    def close(self):
+        if self.server:
+            self.conn.close() 
 
 if __name__ == "__main__":
     intra = intranode()
