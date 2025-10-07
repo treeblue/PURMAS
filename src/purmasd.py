b0273f8782d02856e48eaec7b8ef21fd68588a5f
HFullwood <treeblue3@gmail.com>
2025-10-07 21:12:56 +0100

    worker daemon, starts listening, and prints file names when ctl says job

---
+++ src/purmasd.py
@@ -0,0 +1,33 @@
+from comms import internode
+
+class worker:
+    def __init__(self):
+        self.node_name = "NAMELESS NODE"
+        self.is_running = True
+        self.status = "UNKOWN"
+        self.commands = {"job": self.job}
+        self.controller_ip = ""
+        # self.port = 25732
+        
+    def start(self):
+        self.comm = internode(server=True)
+        self.comm.start()
+        self.comm.bind()
+        self.comm.listen()
+        while True:
+            self.comm.accept()
+            cmd = self.comm.read()
+            if cmd in self.commands:
+                self.commands[cmd]()
+            self.comm.close()
+        self.comm.kill()
+
+    def job(self):
+        self.comm.write("next")
+        file = self.comm.read()
+        self.comm.write("thanks")
+        print(file)
+
+if __name__ == "__main__":
+    w = worker()
+    w.start()
